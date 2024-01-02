7a9802911e7ec8553a32762632db3cfe71deaeaf
timothyrusso <russotimothy@live.it>
2024-01-02 13:44:06 +0100

    feat: layout of the number input field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from '../components/PrimaryButton';
 const StartGameScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -26,5 +32,16 @@ const styles = StyleSheet.create({
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: '#DDB52F',
+    borderBottomWidth: 2,
+    color: '#DDB52F',
+    marginVertical: 8,
+    fontWeight: 'bold',
+    textAlign: 'center',
   },
 });
