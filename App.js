afcd8f81d5a706aa6520d6db03c0705c53aad563
timothyrusso <russotimothy@live.it>
2024-01-03 15:47:13 +0100

    feat: add custom fonts

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import AppLoading from 'expo-app-loading';
+import { useFonts } from 'expo-font';
 import { LinearGradient } from 'expo-linear-gradient';
 import { useState } from 'react';
 import { ImageBackground, SafeAreaView, StyleSheet } from 'react-native';
@@ -9,6 +11,13 @@ import StartGameScreen from './screens/StartGameScreen';
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [gameIsOver, setGameIsOver] = useState(true);
+
+  const [fontsLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) return <AppLoading />;
 
   const pickedNumberHandler = (pickedNumber) => {
     setUserNumber(pickedNumber);
